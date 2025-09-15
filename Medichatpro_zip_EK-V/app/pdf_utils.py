f2c1fa3e9dde6bcfc8329a77f16130a3193fd040
anujg942541-ops <anujg942541@gmail.com>
2025-09-15 18:22:58 +0530

    Update pdf_utils.py

--- Medichatpro_zip_EK-V/app/pdf_utils.py
+++ Medichatpro_zip_EK-V/app/pdf_utils.py
@@ -1,10 +1,13 @@
-from pypdf import  PdfReader
-from typing import List, Optional
-from io import BytesIO
+import fitz  # PyMuPDF
 
-def extract_text_from_pdf(file):
-    reader = PdfReader(file)
-    text = ' '
-    for page in reader.pages:
-        text = text + page.extract_text() or ''
+def extract_text_from_pdf(uploaded_file):
+    """
+    Extract text from a Streamlit-uploaded PDF file.
+    """
+    text = ""
+    # Reset pointer (important if file.read() was called before)
+    uploaded_file.seek(0)
+    with fitz.open(stream=uploaded_file.read(), filetype="pdf") as doc:
+        for page in doc:
+            text += page.get_text("text")
     return text
